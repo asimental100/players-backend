51188826c3c6f7d7e5cf43516c7bb55adafa63d5
Adrian Simental <asimental100@hotmail.com>
2020-08-17 09:33:58 -0700

    fixed relations

--- data/create-tables.js
+++ data/create-tables.js
@@ -16,15 +16,15 @@ async function run() {
                 );
                 CREATE TABLE positions (
                   id SERIAL PRIMARY KEY NOT NULL,
-                  name VARCHAR(256) NOT NULL,
+                  name VARCHAR(256) NOT NULL
                 );
                 CREATE TABLE players (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(256) NOT NULL,
                     age INTEGER NOT NULL,
                     injured VARCHAR(256) NOT NULL,
-                    position_id INTEGER NOT NULL REFERENCES positions(id)
-                    owner_id INTEGER NOT NULL REFERENCES users(id),
+                    position_id INTEGER NOT NULL REFERENCES positions(id),
+                    owner_id INTEGER NOT NULL REFERENCES users(id)
                 );
         `);
     console.log('create tables complete', getEmoji(), getEmoji(), getEmoji());
